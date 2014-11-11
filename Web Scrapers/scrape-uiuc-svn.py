8d9c4cc6d9f4d14f1254ff8b141d7a9a11d2d03b
Andy <andyv.42@gmail.com>
2014-11-11 17:56:43 -0600

    Updated. Working on general file scraper.

--- Web Scrapers/scrape-uiuc-svn.py
+++ Web Scrapers/scrape-uiuc-svn.py
@@ -1,29 +1,45 @@
-import pymongo
-import elasticsearch
-from elasticsearch import Elasticsearch
-from pymongo import MongoClient
 from lxml import html
+from lxml import etree
 import requests
-
+import urllib.request
+import sys
+import re
+import os
 
 ######## Functions ########
-def getFiles(param_url):
-    print(">Running getFiles script.")
+def getFiles(param_url, path):
+  
+    response = urllib.request.urlopen(param_url)
+  
+    # Extract and print response meta-data
+    header = response.info()
     
-    url = param_url
-    print(">Accessing: " + url)
+    # Print the response to console
+    print("URL:",response.geturl())
+    print("Status:", response.getcode())
+    print(header)
+
+    # Extract content
+    data = response.read()
+    data_tree = etree.HTML(data)
     
-    page = requests.get(url)
-    tree = html.fromstring(page.content)
-    filename = tree.xpath('//a/@href')
+    # Extract all the anchors
+    data_tree_links = data_tree.xpath('//a/@href')
+    #print(data_tree_links)
 
-    print(">Printing results of extract:")
-    for n in range(0, len(filename)-1):
-        print("+1")
-        print(filename[n])
+    # Download files
+    #dest_directory = os.path.join(os.path.dirname(os.path.abspath(_file_)), "test")
+    #urllib.request.urlretrieve("http://www.andyvuong.me/resources/avresume.pdf", os.path.join("test", "file.pdf"))
 
-    print(">This script is incomplete. Script ending.")
+    
+    for n in range(0, len(data_tree_links)-1):
+        print(data_tree_links[n])
+        
 
+    response.close()
+
+    
 ######## Run Script ########
         
-getFiles("https://subversion.ews.illinois.edu/svn/fa14-cs241/_shared/past-lectures/")
+getFiles("https://wiki.cites.illinois.edu/wiki/display/cs233fa14/Assignments", "test")
+#getFiles("http://en.wikipedia.org/wiki/Wormhole")
